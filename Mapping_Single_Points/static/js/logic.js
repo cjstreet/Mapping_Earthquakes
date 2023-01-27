45a44ae7a4d3f626299ed767c3a8f942bc29de10
Christy Street <christystreet@Christys-Air.hsd1.co.comcast.net>
2023-01-27 12:43:46 -0700

    updating single points

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -3,19 +3,14 @@ console.log("working");
 
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
-
-// Get data from cities.js
-let cityData = cities;
-
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city)
-    L.marker(city.location)
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString()+ "</h3>")
-  .addTo(map);
-});
+//  Add a marker to the map for Los Angeles, California.
+//  let marker = L.marker([34.0522, -118.2437]).addTo(map);
+/* L.circle([34.0522, -118.2437], {
+    radius: 100
+ }).addTo(map);
+ */
  L.circleMarker([34.0522, -118.2437], {
-    radius: city.population/100000,
+    radius: 300,
     color: "black",
     fillColor: '#ffffa1'
  }).addTo(map);
